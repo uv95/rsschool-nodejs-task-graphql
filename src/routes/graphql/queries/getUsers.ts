7ee8b68c3efedf8f8c8cf74eaa47522673056c5a
Uliana <ulianav95v@gmail.com>
2025-05-24 11:58:46 -0600

    feat: add parse resolve info

--- src/routes/graphql/queries/getUsers.ts
+++ src/routes/graphql/queries/getUsers.ts
@@ -1,6 +1,8 @@
-import { PrismaClient } from '@prisma/client';
-import { GraphQLList } from 'graphql';
+import { PrismaClient, User } from '@prisma/client';
+import { GraphQLList, GraphQLResolveInfo } from 'graphql';
+import { parseResolveInfo, ResolveTree } from 'graphql-parse-resolve-info';
 import { UserType } from '../types/user.js';
+import { UserLoaders } from '../dataloaders/userLoaders.js';
 
 export const GetUsersQuery = {
   type: new GraphQLList(UserType),
@@ -9,10 +11,26 @@ export const GetUsersQuery = {
     args,
     {
       prisma,
+      loaders,
     }: {
       prisma: PrismaClient;
+      loaders: UserLoaders;
     },
+    resolveInfo: GraphQLResolveInfo,
   ) {
-    return await prisma.user.findMany();
+    const parsedInfo = parseResolveInfo(resolveInfo) as ResolveTree;
+    const includeSubscribedToUser =
+      !!parsedInfo?.fieldsByTypeName?.User?.subscribedToUser;
+    const includeUserSubscribedTo =
+      !!parsedInfo?.fieldsByTypeName?.User?.userSubscribedTo;
+
+    const users = await prisma.user.findMany({
+      include: {
+        subscribedToUser: includeSubscribedToUser,
+        userSubscribedTo: includeUserSubscribedTo,
+      },
+    });
+
+    return users;
   },
 };
